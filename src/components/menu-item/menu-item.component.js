d87bea18b43582e613452e63d860a87de3170ecc
Philip Noebauer <pnoebauer>
2020-09-13 10:03:44 +0800

    broken homepage into smaller, dynamic components and updated styles

---
+++ src/components/menu-item/menu-item.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './menu-item.styles.scss';
+
+const MenuItem = ( { title, id, imageUrl, size } ) => {
+	return (
+		<div 
+			className = {`menu-item ${size}`}
+			key = {id} 
+			style={{
+				backgroundImage: `url(${imageUrl})`
+			}}
+		>
+			<div className = 'content'>
+				<h1 className = 'title'>{title}</h1>
+				<span className = 'subtitle'>SHOP NOW</span>
+			</div>
+		</div>
+	)
+};
+
+export default MenuItem;
