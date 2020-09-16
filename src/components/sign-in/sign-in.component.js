4522c3b0fecde3d4df3bd225299c8e98091cd1f5
Philip Noebauer <pnoebauer>
2020-09-16 11:39:19 +0800

    added a custom form-input component to replace default input in the sign-in form

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+
+import './sign-in.styles.scss';
+
 class SignIn extends React.Component {
 	constructor(props) {
 		super(props);
@@ -8,48 +12,47 @@ class SignIn extends React.Component {
 			email: '',
 			password: ''
 		}
-	}
+	};
 
 	handleSubmit = event => {
 		event.preventDefault();
 		this.setState({email: '', password: ''});
-	}
+	};
 
 	handleChange = event => {
 		const { name, value } = event.target;
 		this.setState({
 			[name]: value
 		});
-	}
+	};
 
 	render() {
 		return (
 			<div className='sign-in'>
 				<h2>I already have an account</h2>
 				<span>Sign in with your email and password</span>
-
 				<form onSubmit={this.handleSubmit}>
-					<input 
+					<FormInput 
 						required 
+						label='Email'
 						type='email' 
 						name='email' 
 						value={this.state.email}
-						onChange={this.handleChange}
+						handleChange={this.handleChange}
 					/>
-					<label>Email</label>
-					<input 
+					<FormInput 
 						required	
+						label='Password'
 						type='password' 
 						name='password'  
 						value={this.state.password}
-						onChange={this.handleChange}
+						handleChange={this.handleChange}
 					/>
-					<label>Password</label>
 					<input type='submit' value='Submit'/>
 				</form>
 			</div>
 		)
 	}
-}
+};
 
 export default SignIn;
