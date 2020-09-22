108e1a692dfdc7a143f0cf356f0900960508773f
Philip Noebauer <pnoebauer>
2020-09-22 12:21:51 +0800

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends React.Component {
 							id: snapShot.id,
 							...snapShot.data()
 						}
-					},() => console.log(this.state));
+					});//,() => console.log(this.state));
 				});
 			}
 			//userAuth === null (sign out)
