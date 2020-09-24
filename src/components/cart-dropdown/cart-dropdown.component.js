8616f71d58082d875fb097fcfcad3053c122f878
Philip Noebauer <pnoebauer>
2020-09-24 15:50:35 +0800

    cart-dropdown to render a message if the cart is empty

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -13,9 +13,12 @@ const CartDropdown = ({ cartItems }) => (
 	<div className='cart-dropdown'>
 		<div className='cart-items'> 
 			{
+				cartItems.length ? 
 				cartItems.map(item => (
 					<CartItem key={item.id} item={item}/>
 				))
+				:
+				<span className='empty-message'>Your cart is empty</span>
 			}
 		</div>
 		<CustomButton> GO TO CHECKOUT</CustomButton>
