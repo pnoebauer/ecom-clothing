c628d27851a9daf37cdddddb4ae49ebaaf226f57
Philip Noebauer <pnoebauer>
2020-09-25 15:00:52 +0800

    redux-persist installed and persistor added to utilise localStorage and remember states after refreshing the website

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 
 ReactDOM.render(
 	<Provider store={store}>
 	  <Router>
-	    <App />
+	  	<PersistGate persistor={persistor}>
+	    	<App />
+	    </PersistGate>
 	  </Router>
   </Provider>,
   document.getElementById('root')
