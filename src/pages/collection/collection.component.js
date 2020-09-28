1f1803c61102737a2806c966352998f8b8ecd386
Philip Noebauer <pnoebauer>
2020-09-28 12:53:19 +0800

    created a curried selector for selecting the collection as specified in the url param; mapped collection strings to integer ids for comparing urlParams with the object ids

--- src/pages/collection/collection.component.js
+++ src/pages/collection/collection.component.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './collection.styles.scss';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-const CollectionPage = ({ match }) => (
-	<div className='collection'>
-		<h2>CATEGORY PAGE</h2>
-	</div>
-);
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
-export default CollectionPage;
+// const CollectionPage = ({ collection, match }) => {
+// 	console.log(collection, match.params.collectionId);
+const CollectionPage = ({ collection }) => {
+	console.log(collection);
+	return (
+		<div className='collection'>
+			<h2>CATEGORY PAGE</h2>
+		</div>
+	);
+}
+
+//ownProps -> props of component that is wrapped into connection
+const mapStateToProps = (state, ownProps) => ({
+	collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
