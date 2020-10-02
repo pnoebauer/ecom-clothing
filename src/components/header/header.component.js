c9419cc5b1e3163e7c78320b1bdc5c49316d7159
Philip Noebauer <pnoebauer>
2020-10-02 10:42:38 +0800

    replaced scss for homepage and header with styled components

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,45 +1,53 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './header.styles.scss';
+import { 
+	HeaderContainer, 
+	LogoContainer, 
+	OptionsContainer, 
+	OptionLink, 
+	OptionDiv 
+} from './header.styles';
 
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component.js';
+
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
 
 const Header = ({ currentUser, hideCartDropdown }) => (
-	<div className='header'>
-		<Link className='logo-container' to='/'>
+	<HeaderContainer>
+		<LogoContainer to='/'>
 			<Logo className='logo'/>
-		</Link>
-		<div className='options'>
-			<Link className='option' to='/shop'> 
+		</LogoContainer>
+		<OptionsContainer>
+			<OptionLink to='/shop'> 
 				SHOP
-			</Link>
-			<Link className='option' to='/shop'> 
+			</OptionLink>
+			<OptionLink to='/shop'> 
 				CONTACT
-			</Link>
+			</OptionLink>
 			{
 				currentUser ?
-				<div className='option' onClick={() => auth.signOut()} >SIGN OUT</div>
+				<OptionDiv onClick={() => auth.signOut()} >SIGN OUT</OptionDiv>
 				:
-				<Link className='option' to='/signin'>SIGN IN</Link>
+				<OptionLink to='/signin'>SIGN IN</OptionLink>
 			}
 			<CartIcon/>
-		</div>
+		</OptionsContainer>
 		{
 			hideCartDropdown ?
 			null 
 			:
 			<CartDropdown />
 		}
-	</div>
+	</HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
+
 import Directory from '../../components/directory/directory.component';
 
-const HomePage = () => { 
-	return (
-	<div className = 'homepage'>
+const HomePage = () => (
+	<HomePageContainer>
 		<Directory />
-	</div>
-	);
-};
+	</HomePageContainer>
+);
 
 export default HomePage;
