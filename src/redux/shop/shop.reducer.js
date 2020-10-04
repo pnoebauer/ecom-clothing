31090bf28418e0326f6cfd7dab710c014f78905b
Philip Noebauer <pnoebauer>
2020-10-04 09:55:25 +0800

    updated the shop reducer for handling the async calls

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,15 +1,29 @@
 import { ShopActionTypes } from './shop.types';
 
 const initialState = {
-	collections: null
+	collections: null,
+	isFetching: false,
+	errorMessage: ''
 };
 
 const shopReducer = (state=initialState, action) => {
 	switch (action.type) {
-		case ShopActionTypes.UPDATE_COLLECTIONS:
+		case ShopActionTypes.FETCH_COLLECTIONS_START:
 			return {
 				...state,
-				collections: action.payload
+				isFetching: true
+			};
+		case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+			return {
+				...state,
+				collections: action.payload,
+				isFetching: false
+			};
+		case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+			return {
+				...state,
+				errorMessage: action.payload,
+				isFetching: false
 			};
 		default:
 			return state;
