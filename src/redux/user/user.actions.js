09d6d84f86a9fb7ae596b65aceaf4c059d79cad2
Philip Noebauer <pnoebauer>
2020-10-05 15:14:37 +0800

    updated user types, actions and reducer for implementing redux-saga

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,8 +1,35 @@
 import { UserActionTypes } from './user.types';
 
-export const setCurrentUser = (payload) => {
-	return {
-		type: UserActionTypes.SET_CURRENT_USER,
-		payload
-	}
-}
+export const setCurrentUser = (payload) => ({
+	type: UserActionTypes.SET_CURRENT_USER,
+	payload
+});
+
+export const googleSignInStart = () => ({
+	type: UserActionTypes.GOOGLE_SIGN_IN_START
+});
+
+export const googleSignInSuccess = user => ({
+	type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+	payload: user
+});
+
+export const googleSignInFailure = error => ({
+	type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+	payload: error
+});
+
+export const emailSignInStart = emailAndPassword => ({
+	type: UserActionTypes.EMAIL_SIGN_IN_START,
+	payload: emailAndPassword
+});
+
+export const emailSignInSuccess = user => ({
+	type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+	payload: user
+});
+
+export const emailSignInFailure = error => ({
+	type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
+	payload: error
+});
