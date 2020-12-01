6ce42621e1c35778e8d65930772eb872b07aab4a
Philip Noebauer <pnoebauer>
2020-12-01 12:40:34 +0800

    replaced App.css with createGlobalStyle from styled-components

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,12 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
+import { GlobalStyle } from './global.styles';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-
 import Header from './components/header/header.component';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
@@ -29,18 +28,19 @@ class App extends React.Component {
 	};
 
   render() {
-  	const { currentUser } = this.props;
-    return (
-      <div>
-      	<Header />
-  			<Switch>
-  				<Route exact path='/' component={HomePage} />
-  				<Route path='/shop' component={ShopPage} />
-  				<Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)} />
-  				<Route exact path='/checkout' component={CheckoutPage} />
-  			</Switch>
-      </div>
-    );
+		const { currentUser } = this.props;
+		return (
+			<div>
+				<GlobalStyle />
+				<Header />
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route path='/shop' component={ShopPage} />
+					<Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)} />
+					<Route exact path='/checkout' component={CheckoutPage} />
+				</Switch>
+			</div>
+		);
 	}
 };
 
