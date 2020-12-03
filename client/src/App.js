ebd2e1a1279cef2252c0db7599547df75dafcf69
Philip Noebauer <pnoebauer>
2020-12-03 12:42:07 +0800

    code splitting with React lazy and Suspense

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { GlobalStyle } from './global.styles';
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
 
 class App extends React.Component {
 
@@ -34,10 +36,12 @@ class App extends React.Component {
 				<GlobalStyle />
 				<Header />
 				<Switch>
-					<Route exact path='/' component={HomePage} />
-					<Route path='/shop' component={ShopPage} />
-					<Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)} />
-					<Route exact path='/checkout' component={CheckoutPage} />
+					<Suspense fallback={<div>Loading...</div>}>
+						<Route exact path='/' component={HomePage} />
+						<Route path='/shop' component={ShopPage} />
+						<Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)} />
+						<Route exact path='/checkout' component={CheckoutPage} />
+					</Suspense>
 				</Switch>
 			</div>
 		);
--- client/src/pages/collection/collection.styles.js
+++ client/src/pages/collection/collection.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/macro';
 
 export const CollectionPageContainer = styled.div`
 	display: flex;
-  	flex-direction: column;
+	flex-direction: column;
 `;
 
 export const TitleContainer = styled.h2`
