36e57d8dcb2279c41a42b1183ac93e3c429b156c
Philip Noebauer <pnoebauer>
2020-12-21 11:52:46 +0800

    adding compression library for gzipping on heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 
 //during testing or development
 if(process.env.NODE_ENV !== 'production') require('dotenv').config(); //load .env into process environment (adds variables from there)
@@ -11,6 +12,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express(); //instantiate new express application
 const port = process.env.PORT || 5000; //heroku sets up process port; during development use port 5000
 
+app.use(compression()); //use gzip compression in the Express app to decrease size of response body
 app.use(bodyParser.json()); //for any requests coming in, process their body tag and convert to json
 app.use(bodyParser.urlencoded({ extended: true })); //url requests that contain incorrect characters (i.e. spaces) are converted to correct ones
 
