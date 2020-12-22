c516093d75b65fbdda01ca817f8633b018815c31
Philip Noebauer <pnoebauer>
2020-12-22 13:16:37 +0800

    upated code to be PWA compliant, added serviceworker, https and updated manifest

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 //during testing or development
 if(process.env.NODE_ENV !== 'production') require('dotenv').config(); //load .env into process environment (adds variables from there)
@@ -15,6 +16,7 @@ const port = process.env.PORT || 5000; //heroku sets up process port; during dev
 app.use(compression()); //use gzip compression in the Express app to decrease size of response body
 app.use(bodyParser.json()); //for any requests coming in, process their body tag and convert to json
 app.use(bodyParser.urlencoded({ extended: true })); //url requests that contain incorrect characters (i.e. spaces) are converted to correct ones
+app.use(enforce.HTTPS({ trustProtoHeader: true })); //always use HTTPS even if request comes from HTTP
 
 app.use(cors()); //allow requests from port 3000 (frontend) to port 5000 (backend)
 
@@ -31,7 +33,12 @@ if(process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
 	if(error) throw error;
 	console.log('Server running on port', port);
-})
+});
+
+//when app requests service-worker.js file from '/service-worker.js' route, then send it
+app.get('/service-worker.js', (req,res) => {
+	res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js')); //service-worker comes with create-react-app and is within the build folder
+});
 
 // frontend will post to route /payment
 app.post('/payment', (req, res) => {
@@ -51,4 +58,4 @@ app.post('/payment', (req, res) => {
 			res.status(200).send( { success: stripeRes });
 		}
 	})
-})
+});
