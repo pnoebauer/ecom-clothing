f59d98577475985a4646c4aaf7ee1a6efe93eb85
Philip Noebauer <pnoebauer>
2021-11-01 12:15:03 +0800

    cart-item test update

--- client/src/components/cart-item/cart-item.test.js
+++ client/src/components/cart-item/cart-item.test.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import CartItem from './cart-item.component';
+import {ItemDetailsContainer} from './cart-item.styles';
 
-it('should render CartItem component', () => {
+describe('CartItem component tests', () => {
 	const mockItem = {
 		imageUrl: 'www.testImage.com',
 		price: 10,
 		name: 'hats',
 		quantity: 2,
 	};
+	const wrapper = shallow(<CartItem item={mockItem} />);
 
-	expect(shallow(<CartItem item={mockItem} />)).toMatchSnapshot();
+	test('render CartItem component', () => {
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	test('display correct info', () => {
+		const {price, quantity, name} = mockItem;
+
+		const priceText = wrapper.find('.price').text();
+		expect(priceText).toEqual(`${quantity} x $${price}`);
+
+		const nameText = wrapper.find('.name').text();
+		expect(nameText).toEqual(`${name}`);
+	});
 });
