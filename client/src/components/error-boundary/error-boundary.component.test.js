fdb7e353f4f306ac244367e20f57118ccdff4cae
Philip Noebauer <pnoebauer>
2021-11-03 19:13:12 +0800

    added tests for error-boundary component

---
+++ client/src/components/error-boundary/error-boundary.component.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {mount} from 'enzyme';
+
+import ErrorBoundary from './error-boundary.component';
+
+describe('ErrorBoundary component tests', () => {
+	const wrapper = mount(
+		<ErrorBoundary>
+			<div>Test</div>{' '}
+		</ErrorBoundary>
+	);
+
+	wrapper.setState({hasErrored: true});
+
+	test('render ErrorBoundary component', () => {
+		expect(wrapper).toMatchSnapshot();
+	});
+});
